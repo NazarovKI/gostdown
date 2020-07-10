2e9bb5002eb11e60dd6660728ec3bdc9a4f88cb9
Dan Aksim <danaksim@gmail.com>
2020-07-10 19:45:06 +0300

    Added --lua-filter to demo Pandoc call

--- pandoc-demo-report.bat
+++ pandoc-demo-report.bat
@@ -1,1 +1,1 @@
-pandoc.exe demo-report-beginning.md demo-main.md demo-report-end.md -o demo-report-raw.docx --filter pandoc-crossref --filter pandoc-citeproc --reference-doc demo-template-report.docx
+pandoc.exe demo-report-beginning.md demo-main.md demo-report-end.md -o demo-report-raw.docx --lua-filter ./linebreaks.lua --filter pandoc-crossref --filter pandoc-citeproc --reference-doc demo-template-report.docx
